921e66bd443ca93ad08ad2b06725013edcb8db68
Rohan James <rohanjames9282@gmail.com>
2022-10-17 15:21:14 +0530

    Add #get_symbol

--- lib/player.rb
+++ lib/player.rb
@@ -12,10 +12,27 @@ class Player
     loop do
       name = gets.chomp.capitalize
       if name.length > 10
-        puts "Please enter a smaller name"
+        puts "Please enter a smaller name."
         next
       else
         self.name = name
+        break
+      end
+    end
+  end
+
+  def get_symbol
+    loop do
+      symbol = gets.chomp
+      case
+      when symbol.length > 1
+        puts 'Please keep your symbol limited to one character.'
+        next
+      when symbol.length < 1 || /\s+/.match(symbol)
+        puts 'Your symbol cannot be empty.'
+        next
+      else
+        self.symbol = symbol
         break
       end
     end
