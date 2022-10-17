004a6a8c3d243ea50a845d0d05af30bf00d40b9d
Rohan James <rohanjames9282@gmail.com>
2022-10-17 14:57:14 +0530

    Add tests for #get_symbol

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,27 +1,56 @@
 require_relative '../lib/player'
 
 describe Player do
+  subject(:player) { described_class.new }
+
+  before do
+    allow(STDOUT).to receive(:puts)
+  end
 
   describe '#get_name' do
-    subject(:player_one) { described_class.new }
 
     context 'When name is longer than 10 characters' do
-      xit 'restarts the loop until valid name is given' do
+      it 'restarts the loop until valid name is given' do
         invalid_name = 'fyodor dostoyevsky'
         valid_name = 'charlie'
-        allow(player_one).to receive(:gets).and_return(invalid_name, valid_name)
-        expect(player_one).to receive(:gets).twice
-        player_one.get_name
+        allow(player).to receive(:gets).and_return(invalid_name, valid_name)
+        expect(player).to receive(:gets).twice
+        player.get_name
       end
     end
 
     context 'When valid name is given' do
-      xit 'sets player.name to the name given' do
-        valid_name = 'charles'
-        allow(player_one).to receive(:gets).and_return(valid_name)
-        player_one.get_name
-        expect(player_one.name).to be_eq(valid_name)
+      it 'sets player.name to the name given' do
+        valid_name = 'Charles'
+        allow(player).to receive(:gets).and_return(valid_name)
+        player.get_name
+        expect(player.name).to eq(valid_name)
       end
     end
   end
+
+  describe '#get_symbol' do
+
+    context 'When symbol given has more than one character' do
+      xit 'restarts the loop until valid symbol is given' do
+        invalid_symbol = 'XD'
+        valid_symbol = 'x'
+        allow(player).to receive(:gets).and_return(invalid_symbol, valid_symbol)
+        expect(player).to receive(:gets).twice
+        player.get_symbol
+      end
+    end
+
+    context 'When symbol is does not contain anything or is just empty space' do
+      xit 'restarts the loop until valid symbol is given' do
+        empty_symbol = ''
+        space = '            '
+        valid_symbol = '0'
+        allow(player).to receive(:gets).and_return(empty_symbol, space, valid_symbol)
+        expect(player).to receive(:gets).exactly(3).times
+        player.get_symbol
+      end
+    end
+  end
+
 end
