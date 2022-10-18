af30a632ef8430d0a667022d7853abb786f73a29
Rohan James <rohanjames9282@gmail.com>
2022-10-18 12:16:07 +0530

    Add tests for #make_grid

---
+++ spec/grid_spec.rb
@@ -0,0 +1,55 @@
+require_relative '../lib/grid'
+
+describe Grid do
+  subject(:game_grid) { described_class.new}
+  before do
+    allow(STDOUT).to receive(:puts)
+  end
+
+  describe '#make_grid' do
+    context 'When called' do
+      it 'each row should be have a length of 7' do
+      game_grid.make_grid
+      row_1 = game_grid.grid[1]
+      row_3 = game_grid.grid[3]
+      expect(row_1.length).to eq(7)
+      expect(row_3.length).to eq(7)
+      end
+
+      it 'elements inside the arrays(rows) should be an empty space' do
+        game_grid.make_grid
+        empty_space = ' '
+        random_ele = game_grid.grid[6][5]
+        expect(random_ele).to eq(empty_space)
+      end
+
+      it 'adds a total of 6 rows only' do
+        game_grid.make_grid
+        total_rows = game_grid.grid.length
+        expect(total_rows).to eq(6)
+      end
+    end
+  end
+
+  describe '#find_ele' do
+    context 'When given valid input' do
+      it 'returns element at the specified location' do
+        game_grid.make_grid
+        valid_input = 'r3c4'
+        expected_result = ' '
+        ele_received = game_grid.find_ele(valid_input)
+        expect(ele_received).to eq(expected_result)
+      end
+    end
+
+    context 'When given invalid input' do
+      it 'returns nil' do
+        expected_result = nil
+        game_grid.make_grid
+        invalid_input = 'r1r7'
+        ele_received = game_grid.find_ele(invalid_input)
+        expect(ele_received).to eq(expected_result)
+      end
+    end
+  end
+end
