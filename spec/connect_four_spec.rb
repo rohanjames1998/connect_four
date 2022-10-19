7da9ea64c22b73d6426b022568e81193968251b1
Rohan James <rohanjames9282@gmail.com>
2022-10-19 14:36:55 +0530

    Add tests for #get_players_symbols

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -2,15 +2,15 @@ require_relative '../lib/connect_four'
 
 describe ConnectFour do
   subject(:new_game) { described_class.new }
-  let(:player_one) { instance_double(Player)}
-  let(:player_two) { instance_double(Player)}
+  let(:player_one) { instance_double(Player) }
+  let(:player_two) { instance_double(Player) }
 
   describe '#get_player_names' do
-    before do
-      allow(player_one).to receive(:name)
-      allow(player_two).to receive(:name)
-    end
     context 'When called' do
+      before do
+        allow(player_one).to receive(:name).and_return('jon')
+        allow(player_two).to receive(:name).and_return('snow')
+      end
       it 'calls #get_name on both players' do
         expect(player_one).to receive(:get_name).once
         expect(player_two).to receive(:get_name).once
@@ -19,14 +19,44 @@ describe ConnectFour do
     end
 
     context 'If player two has the same name as player one' do
-      xit 'asks for a different name until it is given' do
-        allow(player_one).to receive(:gets).and_return('jon')
-        allow(player_two).to receive(:gets).and_return('jon', 'snow')
+      before do
+        allow(player_one).to receive(:name).and_return('jon')
+        allow(player_two).to receive(:name).and_return('jon', 'robins')
+      end
+      it 'asks for a different name until it is given' do
+        expect(player_one).to receive(:get_name).once
         expect(player_two).to receive(:get_name).twice
-        new_game.get_players_names
+        new_game.get_players_names(player_one, player_two)
       end
     end
   end
+
+  describe '#get_players_symbols' do
+    before do
+      allow(player_one).to receive(:symbol).and_return('x')
+      allow(player_two).to receive(:symbol).and_return('y')
+    end
+    context 'When called' do
+      xit 'calls #get_symbol on both players' do
+        expect(player_one).to receive(:get_symbol).once
+        expect(player_two).to receive(:get_symbol).once
+        new_game.get_players_symbols(player_one, player_two)
+      end
+    end
+
+    context 'If player two has the same symbol as player one' do
+      before do
+        allow(player_one).to receive(:symbol).and_return('x')
+        allow(player_two).to receive(:symbol).and_return('x', 'y')
+      end
+      xit 'asks for a different symbol until one is given' do
+        expect(player_one).to receive(:get_symbol).once
+        expect(player_two).to receive(:get_symbol).twice
+        new_game.get_players_symbols(player_one, player_two)
+      end
+    end
+  end
+
 
   describe '#play_game' do
     context 'When called' do
