b6b218e57814ea29d834fb5471d45bab5e04ac40
Rohan James <rohanjames9282@gmail.com>
2022-10-19 13:53:06 +0530

    Add tests for #get_players_names

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,22 +1,50 @@
 require_relative '../lib/connect_four'
 
 describe ConnectFour do
+  subject(:new_game) { described_class.new }
+  let(:player_one) { instance_double(Player)}
+  let(:player_two) { instance_double(Player)}
 
-  describe '#start_game' do
-    subject(:new_game) { described_class.new }
-
+  describe '#get_player_names' do
+    before do
+      allow(player_one).to receive(:name)
+      allow(player_two).to receive(:name)
+    end
     context 'When called' do
-      xit 'calls #round for both players' do
-      allow(new_game).to receive(:round).twice
-      expect(new_game).to receive(:round).twice
-      new_game.start_game
+      it 'calls #get_name on both players' do
+        expect(player_one).to receive(:get_name).once
+        expect(player_two).to receive(:get_name).once
+        new_game.get_players_names(player_one, player_two)
       end
+    end
 
-      xit 'breaks the loop if #end_game returns true' do
-        allow(new_game).to receive(:end_game?).and_return(false, true)
-        expect(new_game).to receive(:round).once
-        new_game.start_game
+    context 'If player two has the same name as player one' do
+      xit 'asks for a different name until it is given' do
+        allow(player_one).to receive(:gets).and_return('jon')
+        allow(player_two).to receive(:gets).and_return('jon', 'snow')
+        expect(player_two).to receive(:get_name).twice
+        new_game.get_players_names
       end
     end
   end
+
+  describe '#play_game' do
+    context 'When called' do
+
+      it 'ends the game if #end_game returns true' do
+        allow(new_game).to receive(:end_game?).and_return(true)
+        expect(new_game).to receive(:round).once
+        new_game.play_game
+      end
+
+      it 'calls #round for both players' do
+      allow(new_game).to receive(:round)
+      allow(new_game).to receive(:end_game?).and_return(false, true)
+      expect(new_game).to receive(:round).twice
+      new_game.play_game
+      end
+    end
+  end
+
+
 end
