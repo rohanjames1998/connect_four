d8a611f6008bb7f0c94aecfce44233facdc418d0
Rohan James <rohanjames9282@gmail.com>
2022-10-22 12:42:59 +0530

    Add #save_game

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,11 +1,11 @@
 require_relative 'player'
 require_relative 'grid'
+require 'json'
 
 class ConnectFour
-
   attr_reader :game_grid, :rounds, :p1, :p2
 
-  def initialize(grid=Grid.new, p1=Player.new, p2=Player.new)
+  def initialize(grid = Grid.new, p1 = Player.new, p2 = Player.new)
     @game_grid = grid
     @rounds = 0
     @p1 = p1
@@ -18,37 +18,37 @@ class ConnectFour
     play_game
   end
 
-  def get_players_names(player_one=p1, player_two=p2)
+  def get_players_names(player_one = p1, player_two = p2)
     player_one.get_name
     loop do
       player_two.get_name
       if player_one.name == player_two.name
         puts 'You cannot have the same name as your partner.',
              'Please enter a different name:'
-             next
-      end
-      break
-    end
-  end
-
-  def get_players_symbols(player_one=p1, player_two=p2)
-    player_one.get_symbol
-    loop do
-      player_two.get_symbol
-      if player_one.symbol == player_two.symbol
-        puts 'You cannot have the same symbol as your partner.',
-        'Please enter a different symbol:'
         next
       end
       break
     end
   end
 
+  def get_players_symbols(player_one = p1, player_two = p2)
+    player_one.get_symbol
+    loop do
+      player_two.get_symbol
+      if player_one.symbol == player_two.symbol
+        puts 'You cannot have the same symbol as your partner.',
+             'Please enter a different symbol:'
+        next
+      end
+      break
+    end
+  end
 
   def play_game
     loop do
       round(p1)
       break if end_game?
+
       round(p2)
       break if end_game?
     end
@@ -63,14 +63,18 @@ class ConnectFour
 
   def get_player_input(player)
     loop do
-      input = gets.chomp
+      input = gets.chomp.downcase
       case
-      when input.length <= 2 || input.length >=5
+      when input == 'save'
+        save_game
+        next
+      when input.length <= 2 || input.length >= 5
         display_input_error
         display_valid_inputs
         next
       when input.length == 4 || input.length == 3
         break if successfully_placed?(input, player)
+
         display_input_error
         next
       end
@@ -91,8 +95,8 @@ class ConnectFour
   # to tell the user about different formats of valid input.
   # Main use of this function is to DRY the code.
   def display_valid_inputs
-   puts "There are two different types of input format supported in this game.
-    Type 1 = R1C1. Here 'R1' is the row coordinate and 'C1' is the column coordinate. Note there 
+    puts "There are two different types of input format supported in this game.
+    Type 1 = R1C1. Here 'R1' is the row coordinate and 'C1' is the column coordinate. Note there
     are no spaces or commas between the characters.
     Type 2 = 1, 1. This is also a valid short hand. Note that each character is
     separated by a single comma.
@@ -107,6 +111,25 @@ class ConnectFour
 
   def end_game?
   end
+
+  private
+
+  def save_game
+    loop do
+      print "\nEnter the name of your save file:"
+      file_name = gets.chomp.downcase
+      file_name += '.json'
+      complete_file_name = File.join('../saved_games', file_name)
+      if File.exists?(complete_file_name)
+        puts 'A save file with that name already exists.',
+             next
+      else
+        File.open(save_file, 'w') do |file|
+          file.write(convert_to_json(p1, p2, game_grid, rounds))
+        end
+        puts "\nYour game has been successfully saved."
+        break
+      end
+    end
+  end
 end
-
-
