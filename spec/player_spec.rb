769de68513c641357159796133b85cf23d7a7c39
Rohan James <rohanjames9282@gmail.com>
2022-10-24 13:33:46 +0530

    Add additional tests for #get_symbol

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,7 +8,6 @@ describe Player do
   end
 
   describe '#get_name' do
-
     context 'When name is longer than 10 characters' do
       it 'restarts the loop until valid name is given' do
         invalid_name = 'fyodor dostoyevsky'
@@ -30,9 +29,8 @@ describe Player do
   end
 
   describe '#get_symbol' do
-
     context 'When symbol given has more than one character' do
-      xit 'restarts the loop until valid symbol is given' do
+      it 'restarts the loop until valid symbol is given' do
         invalid_symbol = 'XD'
         valid_symbol = 'x'
         allow(player).to receive(:gets).and_return(invalid_symbol, valid_symbol)
@@ -42,15 +40,23 @@ describe Player do
     end
 
     context 'When symbol is does not contain anything or is just empty space' do
-      xit 'restarts the loop until valid symbol is given' do
+      it 'restarts the loop until valid symbol is given' do
         empty_symbol = ''
-        space = '            '
+        space = '  '
         valid_symbol = '0'
         allow(player).to receive(:gets).and_return(empty_symbol, space, valid_symbol)
         expect(player).to receive(:gets).exactly(3).times
         player.get_symbol
       end
     end
-  end
 
+    context 'When valid symbol is given' do
+      it 'sets player.symbol to valid symbol' do
+        valid_symbol = '@'
+        allow(player).to receive(:gets).and_return(valid_symbol)
+        player.get_symbol
+        expect(player.symbol).to eq(valid_symbol)
+      end
+    end
+  end
 end
